6fd8da801249cc0ceb0c9b735f183ac30f0f2179
Sachini-Dissanayaka <dmsachiniacc@gmail.com>
2021-09-19 17:50:12 +0530

    Updated comments

--- src/Bus.java
+++ src/Bus.java
@@ -14,19 +14,23 @@ public class Bus implements Runnable {
     @Override
     public void run() {
         try {
-            sharedResources.mutex.acquire();
+            sharedResources.mutex.acquire();        //bus locks the mutex
             System.out.println("BUS ARRIVED AT "+java.time.LocalTime.now()+" !!! \n");
             System.out.println("Riders count wait for Bus : "+ sharedResources.waiting );
             System.out.println("Riders count who can board to Bus : "+ Math.min(sharedResources.waiting.get(),50) );
             int maxRidersToBoard = Math.min(sharedResources.waiting.get(), sharedResources.max_seats);
-            for (int i = 0; i < maxRidersToBoard; i++){
+            for (int i = 0; i < maxRidersToBoard; i++){     //A loop to get all the available passengers on board
                 sharedResources.bus=this;
-                sharedResources.busWait.release();
-                sharedResources.boarded.acquire();
+                sharedResources.busWait.release();      //bus signals that it has arrived and can take a passenger on board
+                sharedResources.boarded.acquire();      //Allows one passengers to get on board
             }
 
+
+          /* If the number of passengers waiting earlier was greater than 50, then the remaining passengers is waiting- 50
+            Else if the number of passengers who were waiting earlier was less than 50, then all of them got on board. therefore the number of remaining passengers is 0.
+          */
             sharedResources.waiting.set(Math.max(sharedResources.waiting.get()-sharedResources.max_seats,0));
-            sharedResources.mutex.release();
+            sharedResources.mutex.release();        //bus unlocks the mutex
 
             depart();
 
--- src/Rider.java
+++ src/Rider.java
@@ -13,16 +13,16 @@ public class Rider implements Runnable {
     public void run() {
         try {
 
-            sharedResources.mutex.acquire();      //rider count protect, avoid new riders when bus is at stop
-            sharedResources.waiting.incrementAndGet();
+            sharedResources.mutex.acquire();      //passenger locks the mutex
+            sharedResources.waiting.incrementAndGet();      //increment the number of waiting passengers by one and this is an atomic instruction
             System.out.println("Rider "+sharedResources.waiting+" arrived at "+java.time.LocalTime.now());
-            sharedResources.mutex.release();
+            sharedResources.mutex.release();       //release the mutex
 
-            sharedResources.busWait.acquire();    //Waiting for bus, lock when boarding so only 1 board at same time
+            sharedResources.busWait.acquire();    //acquire the bus semaphore to get on board
 
             board();
-            sharedResources.bus.loaded+=1;
-            sharedResources.boarded.release();
+            sharedResources.bus.loaded+=1;          //get the count of loaded passengers
+            sharedResources.boarded.release();      //once boarded, release the boarded semaphore
 
         } catch (InterruptedException e) {
             e.printStackTrace();
